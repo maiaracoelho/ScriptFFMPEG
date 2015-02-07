b86ded3472154c92f9fb293bc25db84167d4256b
dash <dash@dashclient-pc>
2015-02-07 17:10:01 -0400

    AAtualizaçao da forma como os gráficos são gerados

--- scripts-FFmpegColeta/Scripts_coleta/coletaBufferBdTxt.py
+++ scripts-FFmpegColeta/Scripts_coleta/coletaBufferBdTxt.py
@@ -10,6 +10,7 @@ import string
 from datetime import datetime, timedelta
 import string
 
+
 #Conectar ao banco
 try:
     connection = MySQLdb.connect(host='localhost', user='root', passwd='mysql',db='dash_db')
@@ -31,31 +32,30 @@ path, logbw,  idExecucao = linha.split()
 path_logsbw = str(path) + "/dash_logs_bw"
 path_logsbw_txt = str(path) + "/txt_logbw"
 path_video_txt = str(path) + "/txt_video"
-path_buffer_txt = str(path) + "/txt_buffer"
 
 nameLogs = str(logbw).split("_")
 nameLogs = nameLogs[0] + ".txt"
 arq.close()
 
-'''#Recuperar o experimento que possui o time igual ao do expeimento anterior - Isso eh pq video e audio sao separados
+#Recuperar o experimento do banco
+cursor.execute ('SELECT * FROM dash_execution WHERE stream = "video"')
+exe = cursor.fetchone()
+
+#Recuperar o experimento que possui o time igual ao do expeimento anterior - Isso eh pq video e audio sao separados
 cursor.execute ('SELECT * FROM dash_execution WHERE execution_time = "%s" AND id != %d' %(execution1[1], execution1[0]) )
 execution2 = cursor.fetchone()
-'''
+
 
 #Recuperar todos os throughputs relacionados ao primeiro experimento
-cursor.execute ('SELECT * FROM dash_throughseg WHERE fk_execution = %d' %int(idExecucao))
+cursor.execute ('SELECT * FROM dash_throughseg WHERE fk_execution = %d' %execution1[0])
 throughs1 = cursor.fetchall()
+print execution1[0]
 
-#Recuperar todos os throughputs relacionados ao primeiro experimento
-cursor.execute ('SELECT * FROM dash_bufferlevel WHERE fk_execution = %d' %int(idExecucao))
-buffers1 = cursor.fetchall()
 
-'''
 #Recuperar todos os throughputs relacionados ao segundo experimento
 cursor.execute ('SELECT * FROM dash_throughseg WHERE fk_execution = %d' %execution2[0])
 throughs2 = cursor.fetchall()
 print execution2[0]
-'''
 
 #Abrir o arquivo de log de variacoes de bw
 arqLogsBw=open(path_logsbw + "/" + logbw,"r")
@@ -93,34 +93,15 @@ for through in throughs1:
         timeInitialThrough1 =  timeInitialThrough1 - initialTimeTotalLogBw
         timeInitialThrough1 =  timeInitialThrough1.total_seconds()
         #Converter de bit/ms para bit/s
-        bitrate = int(through[9])
-        bitrate = bitrate + 100
+        bitrate = int(through[9]) + 100
 
         arqLogsBrTxt.write(str(timeInitialThrough1) + " " +  str(bitrate) + "\n")   
         print timeInitialThrough1, bitrate
         
 arqLogsBrTxt.close()
 
-#Criar o arquivo txt para gravar os tempos e os bit rates do video e somar com o bitrate do audio
-arqLogsBfTxt = open(path_buffer_txt + "/" + nameLogs , 'w')
-
-#Tratar tempo para o throughput1 
-for buffer in buffers1:
-    timeInitialBuffer1 = datetime.strptime(buffer[1], '%Y-%m-%dT%H:%M:%S.%fZ')
-    timeInitialBuffer1 = timeInitialBuffer1 + timedelta(hours=-4)
-    if (timeInitialBuffer1 > initialTimeTotalLogBw):
-        timeInitialBuffer1 =  timeInitialBuffer1 - initialTimeTotalLogBw
-        timeInitialBuffer1 =  timeInitialBuffer1.total_seconds()
-        level = buffer[2]
-
-        arqLogsBfTxt.write(str(timeInitialBuffer1) + " " +  str(level) + "\n")   
-        print timeInitialBuffer1, level
-        
-arqLogsBfTxt.close()
-
-'''
 #Tratar tempo para o throughput2
-for through in throughs2:
+'''for through in throughs2:
     timeInitialThrough2 = datetime.strptime(through[3], '%Y-%m-%dT%H:%M:%S.%fZ')
     timeInitialThrough2 = timeInitialThrough2 + timedelta(hours=-4)
     if (timeInitialThrough2 > initialTimeLogBw):
@@ -129,7 +110,9 @@ for through in throughs2:
         #Converter de bit/ms para bit/s
         bitrate = int(through[9]) * 1000             
         print timeInitialThrough2, bitrate
-
 '''
+        
+
+
     
 
--- scripts-FFmpegColeta/Scripts_coleta/coletaTaxasTxtGraph.py
+++ scripts-FFmpegColeta/Scripts_coleta/coletaTaxasTxtGraph.py
@@ -1,4 +1,4 @@
-'''
+﻿'''
 Created on 04/02/2015
 
 @author: dash
@@ -8,8 +8,9 @@ Created on 04/02/2015
 import os
 import time
 import string
-import csv
 from operator import itemgetter
+from os import system, remove
+import Gnuplot, Gnuplot.funcutils
 
 arq = open("entrada_diretorio_captura.txt","r")
 linha = arq.readline()
@@ -17,17 +18,44 @@ path, logbw,  idExecucao = linha.split()
 path_logsbw = str(path) + "/dash_logs_bw"
 path_logsbw_txt = str(path) + "/txt_logbw"
 path_video_txt = str(path) + "/txt_video"
+path_graphs = str(path) + "/graphs"
 arq.close()
 
 #listar todos os arquivos destes diretorios e colocar em listas
 txtLogsBwFiles = os.listdir(path_logsbw_txt)
-txtLogsBrfiles = os.listdir(path_video_txt)
+#txtLogsBwFiles = txtLogsBwFiles.sort()
 
-#criar o diretorio dos graficos
-pathGraph=path+"/graphs"
-os.mkdir(pathGraph) 
+gplot = Gnuplot.Gnuplot(debug = 1)
 
-os.system()
+for txtLogsBwFile in txtLogsBwFiles:
+    txtLogsBwFilewithPath = path_logsbw_txt + "/" + txtLogsBwFile
+    txtLogsBrFilewithPath = path_video_txt + "/" + txtLogsBwFile
+    print txtLogsBwFilewithPath
+    print txtLogsBrFilewithPath
+    title = txtLogsBwFile.split(".")
+    title = title[0]
+    gplot.title("'"+title+"'")
+    gplot.xlabel('Tempo (s)')
+    gplot.ylabel('Largura de Banda (kbps)')
+    gplot('set ytics ( 200, 1600, 2200, 2800, 3400, 4000, 5000)')
+    gplot('set xtics 90')
+    gplot('set xrange [0:900]')
+    gplot('set yrange [0:5000]')
+    gplot('set t png')
+    gplot('set o "'+path_graphs+'/'+title+'.png"')
+    gplot.plot("'"+txtLogsBwFilewithPath+"' u 1:2 w steps t 'Largura de Banda (kbps)', '"+txtLogsBrFilewithPath+"'u 1:2 w p t 'Taxa de bits (kbps)'")
+   
+
+'''system('set ytics ( 200000, 1600000, 2200000, 2800000, 3400000, 4000000, 5000000)')
+system('set xtics 90')
+system('set xlabel "Tempo (s)"')
+system('set ylabel "(bps)"')
+system('set yrange [0:5000000]')
+system('set ylabel "(bps)"')
 
 
-    
+system('gnuplot %s.gp'%(txtLogsBwFile))
+system('set t push')
+system('set t png')
+system('set o %s'%(txtLogsBwFile))
+remove(path_graphs+"/"+'%s.gp'%(txtLogsBwFile))'''
